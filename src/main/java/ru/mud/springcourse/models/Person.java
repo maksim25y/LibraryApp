414870a301b665314ea831b0f88e82e1ee66ef00
mudan <myudanov223@gmail.com>
2024-02-02 21:09:39 +0300

    added page index, added database, added controller PeopleController and PersonDao, method get for address /people

--- src/main/java/ru/mud/springcourse/models/Person.java
+++ src/main/java/ru/mud/springcourse/models/Person.java
@@ -1,50 +1,14 @@
 package ru.mud.springcourse.models;
 
-import jakarta.validation.constraints.*;
+import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
+@Component
 public class Person {
     private int id;
-    @NotEmpty(message = "Name should not be empty")
-    @Size(min = 2,max = 30,message = "name should be between 2 and 30")
-    private String name;
-    @Min(value = 0,message = "Age should be greater then 0")
-    private int age;
-    @NotEmpty(message = "Email should not be empty")
-    @Email(message = "Email is not corrected")
-    private String email;
-
-    //Страна,Город,Индекс человека(состоит из 6 цифр)
-    //Russia, Moskow, 901232
-    @Pattern(regexp = "[A-Z]\\w+, [A-Z]\\w+, \\d{6}",
-            message = "Your address should be in this format: Country, City, Postel Code (6 digits)")
-    private String address;
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Person(int id, String name, int age, String email, String address) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.email = email;
-        this.address = address;
-    }
-    public Person() {
-
-    }
+    private String info;
+    private int birthday;
 
     public int getId() {
         return id;
@@ -54,19 +18,28 @@ public class Person {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getInfo() {
+        return info;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setInfo(String info) {
+        this.info = info;
     }
 
-    public String getAddress() {
-        return address;
+    public int getBirthday() {
+        return birthday;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
+    public void setBirthday(int birthday) {
+        this.birthday = birthday;
+    }
+
+    public Person() {
+    }
+
+    public Person(int id, String info, int birthday) {
+        this.id = id;
+        this.info = info;
+        this.birthday = birthday;
     }
 }
