f966bbd0845d56e7c223a501441e12ad0c245755
mudan <myudanov223@gmail.com>
2024-02-03 15:10:10 +0300

    added description who have book now, but with validation of null

--- src/main/java/ru/mud/springcourse/controllers/BooksController.java
+++ src/main/java/ru/mud/springcourse/controllers/BooksController.java
@@ -8,15 +8,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.mud.springcourse.dao.BookDao;
+import ru.mud.springcourse.dao.PersonDao;
+import ru.mud.springcourse.models.Book;
 
 @Controller
 @Component
 @RequestMapping("/books")
 public class BooksController {
     private final BookDao bookDao;
+    private final PersonDao personDao;
     @Autowired
-    public BooksController(BookDao bookDao) {
+    public BooksController(BookDao bookDao, PersonDao personDao) {
         this.bookDao = bookDao;
+        this.personDao = personDao;
     }
 
     @GetMapping
@@ -26,7 +30,12 @@ public class BooksController {
     }
     @GetMapping("/{id}")
     public String getBook(@PathVariable("id")int id,Model model){
-        model.addAttribute("book",bookDao.getById(id));
+        Book book = bookDao.getById(id);
+        model.addAttribute("book",book);
+        if(book.getUserId()==0)
+            model.addAttribute("people",personDao.index());
+        else
+            model.addAttribute("person",personDao.getById(book.getUserId()));
         return "books/show";
     }
 }
